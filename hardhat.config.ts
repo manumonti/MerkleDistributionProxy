bcd0cc00774592bf444e2938df67131a722b2529
Manuel Montenegro <manuel@nucypher.com>
2023-02-08 09:57:13 +0100

    Add forking support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import {config as dotEnvConfig} from "dotenv";
+
+dotEnvConfig();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        // forking is enabled only if RPC_ENDPOINT env is provided
+        // RPC node must be an archive node (Alchemy recommended)
+        enabled: !!process.env.RPC_ENDPOINT,
+        url: process.env.RPC_ENDPOINT || "",
+        blockNumber: 16582928,
+      }
+    }
+  }
 };
 
 export default config;
