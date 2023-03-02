e80141b70e410cec5d648882d826786ebb4da660
Manuel Montenegro <manuel@nucypher.com>
2023-03-02 11:39:33 +0100

    Stablish compiler version to 0.8.9

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,13 @@ const config: HardhatUserConfig = {
         version: "0.5.5"
       },
       {
-        version: "0.8.17"
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       }
     ],
     overrides: {
